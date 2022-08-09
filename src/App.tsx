9ae528c37188e53d240f3cc8571b125f574a434c
Paula Santos <s.paula@ua.pt>
2022-08-09 14:56:32 +0100

    Handle routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { Switch, Route, Router } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import Main from "./components/Main/Main";
 
 export default ({ history }: { history: any }): React.ReactElement => {
   return (
     <Router history={history}>
       <Switch>
+        <Route exact path="/auth/sign-in" component={SignIn} />
+        <Route exact path="/auth/sign-up" component={SignUp} />
         <Route exact path="/" component={Main} />
       </Switch>
     </Router>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useHistory } from "react-router-dom";
 
 const Main = () => {
-    return <div>THIS IS THE MAIN AUTH COMPONENT</div>
+    const history = useHistory();
+    return <div>THIS IS THE MAIN AUTH COMPONENT
+
+      <button onClick={() => history.push("/auth/sign-in")}>Sign in</button>
+
+      <button onClick={() => history.push("/auth/sign-up")}>Sign up</button>
+    </div>
 }
 export default Main;
